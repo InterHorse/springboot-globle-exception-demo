8a32e822eb4c0c73f2370ed3dbed24c97fa33450
MaYuzhe <mayuzhe1222@qq.com>
2018-11-29 22:29:14 +0800

    feat: Project init.

---
+++ src/main/java/pers/myz/globleexception/springbootglobleexceptiondemo/SpringbootGlobleExceptionDemoApplication.java
@@ -0,0 +1,12 @@
+package pers.myz.globleexception.springbootglobleexceptiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGlobleExceptionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGlobleExceptionDemoApplication.class, args);
+	}
+}
